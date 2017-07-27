15866731a81f0557efae27496f51f117a9231533
Aleksandar Ivanov <smichess@gmail.com>
2017-07-27 15:16:08 +0300

    stomp client has correct callback method types

--- src/main/java/def/stompjs/stompjs/Client.java
+++ src/main/java/def/stompjs/stompjs/Client.java
@@ -1,8 +1,8 @@
 package def.stompjs.stompjs;
 import def.js.Function;
 public class Client extends def.js.Object {
-    native public Object connect(String user, String pass, Function callback, Object errCallback);
-    native public Subscription subscribe(String pipe, Function onMessage);
+    native public Object connect(String user, String pass, java.util.function.Consumer<?> callback, java.util.function.Consumer<?> errCallback);
+    native public Subscription subscribe(String pipe, java.util.function.Consumer onMessage);
     native public Object send(String pipe, Options options, Object message);
     public Heartbeat heartbeat;
 }
