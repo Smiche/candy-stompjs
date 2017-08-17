a528393ea51bfd66759d21597cb1af7d6ae33236
Aleksandar Ivanov <smichess@gmail.com>
2017-08-17 16:31:58 +0300

    adding disconnect method

--- src/main/java/def/stompjs/stompjs/Client.java
+++ src/main/java/def/stompjs/stompjs/Client.java
@@ -1,8 +1,8 @@
 package def.stompjs.stompjs;
-import def.js.Function;
 public class Client extends def.js.Object {
     native public Object connect(String user, String pass, java.util.function.Consumer<?> callback, java.util.function.Consumer<?> errCallback);
     native public Subscription subscribe(String pipe, java.util.function.Consumer<Message> onMessage);
+    native public void disconnect();
     native public Object send(String pipe, Options options, Object message);
     public Heartbeat heartbeat;
 }
