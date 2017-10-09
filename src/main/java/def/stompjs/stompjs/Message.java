4e2ef23a3741ccd9cf1ce92c114db1024a3b66fc
Aleksandar Ivanov <smichess@gmail.com>
2017-10-09 10:14:50 +0300

    changing headers to a js object and adding a map getter

--- src/main/java/def/stompjs/stompjs/Message.java
+++ src/main/java/def/stompjs/stompjs/Message.java
@@ -1,4 +1,7 @@
 package def.stompjs.stompjs;
+
+import def.js.Map;
+
 public class Message extends def.js.Object {
     public Message(){}
     
@@ -7,7 +10,7 @@ public class Message extends def.js.Object {
 
     //TODO: change to Map<String,String> somehow
     @jsweet.lang.Optional
-    public Object headers;
+    public def.js.Object headers;
 
     @jsweet.lang.Optional
     public String body;
@@ -17,5 +20,19 @@ public class Message extends def.js.Object {
 
     @jsweet.lang.Optional
     public def.js.Function nack;
+    
+    public Map<String, String> getHeadersAsStrings(){
+    	Map<String, String> map = new Map<String, String>();
+    	
+    	if(this.headers == null) {
+    		return null;
+    	}
+    		
+    	for(def.js.String key : def.js.Object.keys(this.headers)) {
+    		map.set(key.toString(), this.headers.$get(key));
+    	}
+    	
+    	return map;
+    }
 }
 
